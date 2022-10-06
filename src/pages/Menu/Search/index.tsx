47994cef61d7b003cdba88f503371c680501b409
Rafael Dias <rafaelldiias100@gmail.com>
2022-10-05 22:42:19 -0300

    Criação do input se pesquisar

---
+++ src/pages/Menu/Search/index.tsx
@@ -0,0 +1,19 @@
+import { CgSearch } from "react-icons/cg";
+import styles from "./Search.module.scss";
+
+interface SearchProps {
+  searchValue: string;
+  setSearchValue: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Search({ searchValue, setSearchValue }: SearchProps) {
+  return (
+    <div className={styles.search}>
+      <input
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+      <CgSearch size={20} color="#4c4d5e" />
+    </div>
+  );
+}
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { ReactComponent as Logo } from "assets/logo.svg";
 import styles from "./Menu.module.scss";
+import Search from "./Search";
 
 export default function Menu() {
+  const [searchValue, setSearchValue] = useState("");
+
   return (
     <main>
-      <nav className={styles.menu}>
+      <nav className={styles.logo}>
         <Logo />
       </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>A casa do código e da massa</div>
       </header>
+      <section className={styles.menu}>
+        <h3 className={styles.menu__title}>Cardápio</h3>
+        <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+      </section>
     </main>
   );
 }
