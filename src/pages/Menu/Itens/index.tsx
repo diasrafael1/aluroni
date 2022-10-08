360d019651254c12b48ba460cae9988b48dd40a8
Rafael Dias <rafaelldiias100@gmail.com>
2022-10-08 15:18:30 -0300

    adicionada funcionalidade de pesquisar e filtrar pratos.

--- src/pages/Menu/Itens/index.tsx
+++ src/pages/Menu/Itens/index.tsx
@@ -1,11 +1,38 @@
+import { useState, useEffect } from "react";
 import itens from "./itens.json";
 import Item from "./Item";
 import styles from "./Itens.module.scss";
 
-export default function Itens() {
+interface Props {
+  searchValue: string;
+  filter: number | null;
+  order: string;
+}
+
+export default function Itens({ searchValue, filter, order }: Props) {
+  const [listItens, setListItens] = useState(itens);
+
+  function testSearch(title: string) {
+    const regex = new RegExp(searchValue, "i");
+    return regex.test(title);
+  }
+
+  function testFilter(id: number) {
+    if (filter) return filter === id;
+    return true;
+  }
+
+  useEffect(() => {
+    const newList = itens.filter(
+      (item) => testSearch(item.title) && testFilter(item.category.id)
+    );
+
+    setListItens(newList);
+  }, [searchValue, filter]);
+
   return (
     <div className={styles.itens}>
-      {itens.map((item) => (
+      {listItens.map((item) => (
         <Item key={item.id} item={item} />
       ))}
     </div>
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -26,7 +26,7 @@ export default function Menu() {
           <Filters filter={filter} setFilter={setFilter} />
           <Order order={order} setOrder={setOrder} />
         </div>
-        <Itens />
+        <Itens searchValue={searchValue} filter={filter} order={order} />
       </section>
     </main>
   );
