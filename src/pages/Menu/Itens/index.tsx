2ef4ba56c399c1a46fb337766afbf2aaeb340bb4
Rafael Dias <rafaelldiias100@gmail.com>
2022-10-08 19:44:28 -0300

    feita ordenação dos pratos.

--- src/pages/Menu/Itens/index.tsx
+++ src/pages/Menu/Itens/index.tsx
@@ -22,13 +22,26 @@ export default function Itens({ searchValue, filter, order }: Props) {
     return true;
   }
 
+  function handleOrder(list: typeof itens): typeof itens {
+    switch (order) {
+      case "porcao":
+        return list.sort((a, b) => (a.size > b.size ? 1 : -1));
+      case "qtd_pessoas":
+        return list.sort((a, b) => (a.serving > b.serving ? 1 : -1));
+      case "preco":
+        return list.sort((a, b) => (a.price > b.price ? 1 : -1));
+      default:
+        return list;
+    }
+  }
+
   useEffect(() => {
     const newList = itens.filter(
       (item) => testSearch(item.title) && testFilter(item.category.id)
     );
 
-    setListItens(newList);
-  }, [searchValue, filter]);
+    setListItens(handleOrder(newList));
+  }, [searchValue, filter, order]);
 
   return (
     <div className={styles.itens}>
